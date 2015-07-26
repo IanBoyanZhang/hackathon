2a0ef3e8f15db9406a704d425c1b017cc5acf7fd
Silly Knight <sillyknight1986@gmail.com>
2015-07-25 17:03:08 -0700

    Setup angular controller

--- app/app.js
+++ app/app.js
@@ -1,9 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute'
+angular.module('HotSpot', [
+  'ngRoute',
+  'HotSpot.view'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/view'});
 }]);
