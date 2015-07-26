4f7ea092741c012988576f0e93dd383944d9889f
Silly Knight <sillyknight1986@gmail.com>
2015-07-25 17:07:14 -0700

    Angular test server setup

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -6,7 +6,7 @@ module.exports = function(app, express) {
 	app.use(morgan('dev'));
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
-	app.use(express.static(__dirname + '/../../app'));
+	app.use(express.static(__dirname + '/../../'));
 	
 	// Set up routing table
 	// app.get("/data", function(req, res, next) {
