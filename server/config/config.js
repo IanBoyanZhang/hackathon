ea08688dbb9de0a405d04f14e8e710292cfe43bd
Alex <aj42869@gmail.com>
2015-07-25 20:48:13 -0700

    [Feature] added lib folder with Uber handlers to server directory

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,15 @@
 // Keys 
 module.exports = {
-	twitterConsumerKey: "MNpmyWfqMv8qCXXWhIlAG84Ok",
-	twitterConsumerSecret: "CS7POWUkMHsHcPgzVCQMtCIb7Vu47vuHHodPrXPe32ag7TIwmp",
-	twitterAccessToken: "3278872357-tvBraRyprrRID0hBI1SvIROb4UjnYE9m39lVMhf",
-	twitterAccessTokenSecret: "p8no5D84NDYJ2NXWI8DVRmwkY7YFIglB3KssasN5ky9tS"
+  twitter: {
+  	twitterConsumerKey: "MNpmyWfqMv8qCXXWhIlAG84Ok",
+  	twitterConsumerSecret: "CS7POWUkMHsHcPgzVCQMtCIb7Vu47vuHHodPrXPe32ag7TIwmp",
+  	twitterAccessToken: "3278872357-tvBraRyprrRID0hBI1SvIROb4UjnYE9m39lVMhf",
+  	twitterAccessTokenSecret: "p8no5D84NDYJ2NXWI8DVRmwkY7YFIglB3KssasN5ky9tS"
+  },
+  uber: {
+    client_id: "i1wKltHiwwoVyJW6gxLCezztn1k5xgYJ",
+    server_token: "r0j5klEzui5Pruxvi80FY-3U1OhoJn1UkjqNmh-l",
+    secret: "OttdwJjgN1juCkTyuvDGA_p24s2qbMMvCOu42dS3"
+  }
 };
+
